7919ddf29772e1ab93a31b05507c0c7cf76261ed
mocha225 <lori@artinspired.me>
2018-10-28 21:51:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,70 @@
+package com.example.android.pets.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+// give it an empty constructor.
+public final class PetContract {
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = ".com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class PetEntry implements BaseColumns {
+
+        public static final String COLUMN_ID = BaseColumns._ID;
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+        public static final String TABLE_NAME = "pets";
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+    /**
+     * Returns whether or not the given gender is {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+     * or {@link #GENDER_FEMALE}.
+     */
+    public static boolean isValidGender(int gender) {
+        if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE) {
+            return true;
+        }
+        return false;
+    }}}
+
+
+
+
+
+
+
+
